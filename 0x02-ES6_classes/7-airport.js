0ec3a616c2215f4746ae992d22aad8d728eb50f2
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-06-27 18:25:22 +0000

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,36 @@
+/**
+ * Represents the Airport class.
+ * @author https://www.github.com/amiroudiallo-dev
+ */
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be string.!');
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be string.!');
+    }
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
