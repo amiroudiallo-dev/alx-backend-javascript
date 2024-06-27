85695ec294abd89829fe27294bbe34b9e285b718
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-06-27 11:05:31 +0000

    0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,11 +1,9 @@
 /**
  * Represents a classRoom.
  */
-
 export default class ClassRoom {
-
     /**
-     * create a new @see {@link ClassRoom}
+     * create a new @see {@link ClassRoom}.
      * @param {Number} maxStudentsSize : The maximum number of student in the class.
      */
     constructor(maxStudentsSize) {
