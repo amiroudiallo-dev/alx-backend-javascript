ac24e101265a2475b1aae7891ec70409479c2ccb
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-06-27 10:54:51 +0000

    0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/**
+ * Represents a classRoom.
+ */
+
+export default class ClassRoom {
+
+    /**
+     * create a new @see {@link ClassRoom}
+     * @param {Number} maxStudentsSize : The maximum number of student in the class.
+     */
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,10 @@
+## ES6 Classes
+Alx backend specialization
+
+## Learning Objectives
+
+- How to define a Class
+- How to add methods to a class
+- Why and how to add a static method to a class
+- How to extend a class from another
+- Metaprogramming and symbols
