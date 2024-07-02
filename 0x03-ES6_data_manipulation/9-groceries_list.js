1317086bebcf040fa9668436c40242ccd97eeb8c
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-07-02 13:36:16 +0000

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * Returns a map of groceries with the following items.
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
