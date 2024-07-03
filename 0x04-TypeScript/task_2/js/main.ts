1f0fc45be0afd9b85c847626052cbab61510e0ec
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-07-03 14:28:56 +0000

    task_2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -43,12 +43,33 @@ export class Teacher implements TeacherInterface {
   workTeacherTasks(): string {
     return 'Getting to work';
   }
+}
 
-  createEmployee(salary: (number | string)): (Director | Teacher) {
-    if (typeof salary === 'number' && salary <= 500) {
-      return new Teacher();
-    }
-    return new Director();
+export function createEmployee(salary: (number | string)): (Director | Teacher) {
+  if (typeof salary === 'number' && salary <= 500) {
+    return new Teacher();
   }
-  
+  return new Director();
+}
+
+export function isDirector(employee: (Director | Teacher)) {
+  return employee instanceof Director;
+}
+
+export function executeWork(employee: (Director | Teacher)) {
+  if (isDirector(employee)) {
+    return (employee as Director).workDirectorTasks();
+  }
+  return (employee as Teacher).workTeacherTasks();
+}
+
+export type Subjects = ('Math' | 'History');
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
 }
