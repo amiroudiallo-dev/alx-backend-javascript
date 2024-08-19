8e6851808ab9796ae5c44b95742d60bb99e673b9
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-08-19 09:29:39 +0000

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Amirou DIALLO <https://github.com/amiroudiallo-dev>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
